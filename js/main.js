2f1a52c0072a9cc8c95f2f8ac5f52cf8367f1ead
kristina-lim <kristinahlim@gmail.com>
2022-10-09 11:36:28 -0700

    making code more readable and adding new code in

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ let winner;
 
 /*----- cached elements  -----*/
 const messageEl = document.querySelector('h2');
-const guessEl = document.querySelector('guess'); //guessEl -> guessL, so that when guessL is clicked, it could go to id guess
+const guessEl = document.querySelector('guess');
 const letterEl = document.querySelector('#letter');
 const playAgainBtn = document.querySelector('#replay');
 
@@ -30,22 +30,22 @@ function init() {
 }
 
 function letterClick(evt) {
-  const letter = evt.target.innerText;
+  guessEl.textContent = guessL;
+  const letter = evt.target.tagName;
   if (
     letter !== 'BUTTON' ||
     guessL.includes(letter) ||
     wrongL.includes(letter) ||
     winner) return;
   
-    winner = getWinner();
+  winner = getWinner();
+  render();
 }
 
 function getWinner() {
-  if (winner === 'L') {
-    guessL === rndWord;
-  }
+  if (winner === 'L') return guessL === rndWord;
   
-  if (rndWord === correctL) {
+  if (rndWord === guessL) {
     return 'W';
   } else if (wrongL === MAX_NUM_CHANCES) {
     return 'L';
@@ -61,18 +61,21 @@ function render() {
 }
 
 function renderLetterColor() {
+  
+
   //use letterEl here so that when guess letter is clicked correctly, it turns green.
   //if letterEl is incorrect though, turn the thing red
   //otherwise, have it turn to the original brown color. (how do i code a hex into js?)
 }
 
 function renderMessage() {
+  const chances = MAX_NUM_CHANCES - wrongL.length;
   if (winner) {
     messageEl.innerText = 'WOW!! CONGRATS ON GUESSING THE RIGHT WORD!';
   } else if (MAX_NUM_CHANCES < wrongL.length) {
     messageEl.innerText = 'OOPS, GAME OVER! GINGY IS BROKEN 😭 TRY AGAIN!';
   } else {
-    messageEl.innerHTML == `${MAX_NUM_CHANCES - wrongL.length} CHANCES LEFT`
+    messageEl.innerHTML == `${chances} CHANCES LEFT`;
   }
 }
 
