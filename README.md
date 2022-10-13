af7931aaa409d61b2f8889fc11105e93ac7f42fa
kristina-lim <kristinahlim@gmail.com>
2022-10-13 11:53:49 -0700

    final edits

--- README.md
+++ README.md
@@ -1,84 +1,81 @@
 <h1>GINGYMAN</h1>
 <h2>Welcome to Kristina's Gingyman!</h2>
 This game is an alternative version of Spaceman. The outcome of this game is to guess a letter and see if the letter is displayed in the underscore(s). There are six chances given, and each incorrect letter guessed will remove a piece of Gingy.
-</br>
-</br>
+<br />
+<br />
 When the player selects a letter, the letter will not be able to be clicked again. All buttons are a shade of yellowish brown and if an incorrect letter is guessed, the button color will change to red. Otherwise, if a correct letter is guessed, the letter will replace the underscore(s), and the button color will change to green. Once a letter is clicked, the player will not be able to click the same letter.
-</br>
-</br>
+<br />
+<br />
 If the player uses all six chances, a message will pop up indicating that the player lost, and the play again button will pop up. Or if the player is able to guess the word, a congratulatory message will pop up, and the play again button will pop up as well. Whether the player won or lost the game, the secret word will be revealed.
 
-</br>
+<br />
 <h2>Screenshots</h2>
 The start of the game, with 6 chances:
-</br>
+<br />
 <img src="imgs/start-game.png">
 
-</br>
+<br />
 When the player chooses the right letter, the button turns green:
-</br>
+<br />
 <img src="imgs/correct-letter.png">
 
-</br>
+<br />
 When the player chooses the wrong letter, the button turns red:
-</br>
+<br />
 <img src="imgs/incorrect-letter.png">
 
-</br>
+<br />
 When the player chooses the first incorrect guess, the number of chances goes down to 5, and Gingy loses a leg:
-</br>
+<br />
 <img src="imgs/chances-5.png">
 
-</br>
+<br />
 Second incorrect guess and the message is displayed with "4 chances left":
-</br>
+<br />
 <img src="imgs/chances-4.png">
 
-</br>
+<br />
 Third incorrect guess and the message is displayed with "3 chances left":
-</br>
+<br />
 <img src="imgs/chances-3.png">
 
-</br>
+<br />
 Fourth incorrect guess and the message is displayed with "2 chances left":
-</br>
+<br />
 <img src="imgs/chances-2.png">
 
-</br>
+<br />
 Fifth incorrect guess and the message is displayed with "1 chances left":
-</br>
+<br />
 <img src="imgs/chances-1.png">
 
-</br>
+<br />
 When the player loses all their chances, Gingy disappears and a lost message shows up on the top:
-</br>
+<br />
 <img src="imgs/lose-message.png">
 
-</br>
+<br />
 When the player wins the game, a congratulatory message shows up on the top:
-</br>
+<br />
 <img src="imgs/win-message.png">
 
-
-</br>
+<br />
 If the player wins or loses the game, the replay button pops up on the bottom:
-</br>
+<br />
 <img src="imgs/replay-button.png">
 
-</br>
+<br />
 <h2>Technologies Used</h2>
 
 * HTML
 * CSS
 * JavaScript
 
-</br>
 <h2>Getting Started</h2>
 Click this link down below to play:
 
-[Play Gingyman](https://kristina-lim.github.io/Gingyman/)
+[Play Gingyman](targetURL "https://kristina-lim.github.io/Gingyman/")
 
-</br>
 <h2>Next Steps (Icebox Items)</h2>
 
 * Make the button(s) change color when clicked: green for correct and red for incorrect.
--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
 /*----- constants -----*/
-const WORD_BANK = ['DREAMWORKS', 'EDDIE MURPHY', 'THE MUFFIN MAN', 'PINOCCHIO', 'SHREK IS LOVE', 'SHREK IS LIFE', 'LORD FARQUAAD', 'DONKEY', 'PRINCESS FIONA', 'PUSS IN BOOTS', 'FAR FAR AWAY', 'SHREK FOREVER AFTER', 'CAMERON DIAZ', 'MIKE MYERS', 'OGRES ARE LIKE ONIONS', 'ALL OGRE NOW', 'YOU CRY YOU LOSE', 'DONT WORRY BE HAPPY', 'JOHN LITHGOW','FAIRY GODMOTHER', 'PRINCE CHARMING', 'QUEEN', 'JULIE ANDREWS', 'BABY DONKEY'];
+const WORD_BANK = [
+  "DREAMWORKS",
+  "EDDIE MURPHY",
+  "THE MUFFIN MAN",
+  "PINOCCHIO",
+  "SHREK IS LOVE",
+  "SHREK IS LIFE",
+  "LORD FARQUAAD",
+  "DONKEY",
+  "PRINCESS FIONA",
+  "PUSS IN BOOTS",
+  "FAR FAR AWAY",
+  "SHREK FOREVER AFTER",
+  "CAMERON DIAZ",
+  "MIKE MYERS",
+  "OGRES ARE LIKE ONIONS",
+  "ALL OGRE NOW",
+  "YOU CRY YOU LOSE",
+  "DONT WORRY BE HAPPY",
+  "JOHN LITHGOW",
+  "FAIRY GODMOTHER",
+  "PRINCE CHARMING",
+  "QUEEN",
+  "JULIE ANDREWS",
+  "BABY DONKEY",
+];
 const MAX_NUM_CHANCES = 6;
 
 /*----- state variables -----*/
@@ -9,15 +34,15 @@ let holdL;
 let winner;
 
 /*----- cached elements  -----*/
-const messageEl = document.querySelector('h2');
-const letterEl = document.querySelectorAll('#letter > button');
-const guessEl = document.getElementById('guess');
-const playAgainBtn = document.getElementById('replay');
-const gingyEl = document.getElementById('gingy');
+const messageEl = document.querySelector("h2");
+const letterEl = document.querySelectorAll("#letter > button");
+const guessEl = document.getElementById("guess");
+const playAgainBtn = document.getElementById("replay");
+const gingyEl = document.getElementById("gingy");
 
 /*----- event listeners -----*/
-document.querySelector('#letter').addEventListener('click', letterClick);
-playAgainBtn.addEventListener('click', init);
+document.querySelector("#letter").addEventListener("click", letterClick);
+playAgainBtn.addEventListener("click", init);
 
 /*----- functions -----*/
 init();
@@ -29,26 +54,29 @@ function init() {
   winner = null;
 
   for (let i = 0; i < word.length; i++) {
-    if (word[i] == ' ') {
-      holdL.push(' ');
+    if (word[i] == " ") {
+      holdL.push(" ");
     } else {
-      holdL.push('_');
+      holdL.push("_");
     }
-  }  
-  guessEl.innerHTML = holdL.join('');
+  }
+  guessEl.innerHTML = holdL.join("");
 
   for (let i = 0; i < letterEl.length; i++) {
-    letterEl[i].style.backgroundColor = '#FFEBC';
+    letterEl[i].style.backgroundColor = "#FFEBC";
   }
   render();
 }
 
 function letterClick(evt) {
   const letter = evt.target.id;
-  if (evt.target.tagName !== 'BUTTON' ||
+  if (
+    evt.target.tagName !== "BUTTON" ||
     holdL.includes(letter) ||
     wrongL.includes(letter) ||
-    winner) return;
+    winner
+  )
+    return;
 
   if (word.includes(letter)) {
     for (let i = 0; i < word.length; i++) {
@@ -56,17 +84,17 @@ function letterClick(evt) {
         holdL.splice(i, 1, letter);
       }
     }
-    guessEl.textContent = holdL.join('');
+    guessEl.textContent = holdL.join("");
   } else {
-      wrongL += letter;
+    wrongL += letter;
   }
   winner = getWinner();
   render();
 }
 
 function getWinner() {
-  if (!holdL.includes('_')) return 'W';
-  if (wrongL.length === MAX_NUM_CHANCES) return 'L';
+  if (!holdL.includes("_")) return "W";
+  if (wrongL.length === MAX_NUM_CHANCES) return "L";
   return null;
 }
 
@@ -77,36 +105,35 @@ function render() {
   gingyEl.src = `imgs/gingy-${wrongL.length}.jpg`;
 }
 
-
 function renderButtonColor() {
   letterEl.forEach((button) => {
     const letterBtn = button.innerText;
     if (wrongL.includes(letterBtn)) {
-      button.style.backgroundColor = '#FFE4E1'; //LightCoral#F08080 //MistyRose#FFE4E1
+      button.style.backgroundColor = "#EF9A9A";
     } else if (holdL.includes(letterBtn)) {
-      button.style.backgroundColor = '#F0FFF0'; //LightGreen#90EE90 //HoneyDew#F0FFF0
+      button.style.backgroundColor = "#C5E1A5";
     } else {
-      button.style.backgroundColor = '#FFEBCD';
+      button.style.backgroundColor = "#FFEBCD";
     }
   });
 }
 
 function renderMessage() {
-  guessEl.innerText === holdL.join('');
-  if (winner === 'L') {
+  guessEl.innerText === holdL.join("");
+  if (winner === "L") {
     guessEl.innerText = word;
   }
-  
+
   const chances = MAX_NUM_CHANCES - wrongL.length;
-  if (winner === 'W') {
-    messageEl.innerText = 'WOW!! CONGRATS ON GUESSING THE RIGHT WORD! ';
-  } else if (winner === 'L') {
-    messageEl.innerText = 'OH NO! GINGY IS BROKEN 😭 TRY AGAIN!';
+  if (winner === "W") {
+    messageEl.innerText = "WOW!! CONGRATS ON GUESSING THE RIGHT WORD! ";
+  } else if (winner === "L") {
+    messageEl.innerText = "OH NO! GINGY IS BROKEN 😭 TRY AGAIN!";
   } else {
     messageEl.innerText = `${chances} CHANCES LEFT`;
   }
 }
 
 function renderControls() {
-  playAgainBtn.style.visibility = winner ? 'visible' : 'hidden';
+  playAgainBtn.style.visibility = winner ? "visible" : "hidden";
 }
